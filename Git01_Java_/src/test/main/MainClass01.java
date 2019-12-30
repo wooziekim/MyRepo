bfc7866ada41fbae3605125f69d2618635248778
kimtaewoo <wooziekim@gmail.com>
2019-12-30 11:06:20 +0900

    lab1 작업완료

--- Git01_Java_/src/test/main/MainClass01.java
+++ Git01_Java_/src/test/main/MainClass01.java
@@ -7,5 +7,6 @@ public class MainClass01 {
 		System.out.println("lab1 브렌치에서 작업함");
 		System.out.println("lab1 브렌치에서 작업함2");
 		System.out.println("lab1 브렌치에서 Hello~");
+		System.out.println("lab1 브렌치에서 작업중...");
 	}
 }
