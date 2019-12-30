6950fd6b23532b37ea3082388796d05e01c954b7
kimtaewoo <wooziekim@gmail.com>
2019-12-30 10:10:26 +0900

    lab1 브렌치에서 작업함2

--- Git01_Java_/src/test/main/MainClass01.java
+++ Git01_Java_/src/test/main/MainClass01.java
@@ -5,5 +5,6 @@ public class MainClass01 {
 		System.out.println("Kimgura");
 		System.out.println("Monkey");
 		System.out.println("lab1 브렌치에서 작업함");
+		System.out.println("lab1 브렌치에서 작업함2");
 	}
 }
